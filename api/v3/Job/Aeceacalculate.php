3cf1ee540c35b524e14bdd09b3ac7c500352e4d0
KarinG <karin@semper-it.com>
2026-08-20 09:43:42 -0600

    Debug job

--- api/v3/Job/Aeceacalculate.php
+++ api/v3/Job/Aeceacalculate.php
@@ -10,14 +10,14 @@
    */
   function civicrm_api3_job_aeceacalculate($params) {
 
-    // Pull up all of >= yesterday's contribution of Financial Type = (see $params) that are Pending or Completed:
+    // Pull up all of >= yesterday's contribution of Financial Type = (see $params) that are Completed:
     // -7 days is regular interval
     $yesterday = date("Y-m-d", strtotime( '-5 days' ) );
     $resultContributions = civicrm_api3('Contribution', 'get', array(
       'sequential' => 1,
       'return' => ["contact_id", "receive_date", "financial_type_id"],
       'financial_type_id' => array($params['financialtype']),
-      'contribution_status_id' => ["Pending", "Completed"],
+      'contribution_status_id' => ["Completed"],
       'receive_date' => array('>=' => $yesterday),
       'options' => array(
         'limit' => 0,
@@ -27,7 +27,10 @@
     $output = [];
     $counter = 0;
     foreach ($resultContributions['values'] as $singleContribution) {
-      $date = $contributionID = $contributionType = $newEndDate = '';
+      $contactId = '';
+      $contributionID = '';
+      $financialTypeId ='';
+      $date = '';
 
       $contactId = $singleContribution['contact_id'];
       $contributionID = $singleContribution['contribution_id'];
@@ -39,8 +42,8 @@
         if ($params['financialtype'] == 'KG Payment') {
           $c = strtotime(date("Y-m-d", strtotime($date)) . " +2 month");
         // }
-        // elseif ($params['financialtype'] == 'Monthly Contributions') {
-        //   $c = strtotime(date("Y-m-d", strtotime($date)) . " +2 month");
+        // elseif ($params['financialtype'] == 'Other Contributions') {
+        //   $c = strtotime(date("Y-m-d", strtotime($date)) . " +3 month");
         };
 
         $newEndDate = date("Y-m-d", $c);
@@ -51,6 +54,7 @@
 
       $createMembership = aeceamembership_calc_Membership($contactId, $newEndDate, $contributionID, $receive_date, $financialTypeId);
       $membershipId = $createMembership['id'];
+      $output[] = $financialTypeId;
       $output[] = ts('Membership for contact id %1 %2 %3 %4 %5', array(1 => $contactId, 2 => $financialTypeId, 3 => $membershipId, 4 => $receive_date, 5 => $newEndDate));
       ++$counter;
     }
